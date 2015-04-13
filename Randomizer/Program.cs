b298b2e08b83b1c4dead8d2779a25c6bddf31174
unknown <jgoldschmidt@JGOLDSCHMIDT.gvdasa.local>
2015-04-13 08:11:55 -0300

    Criado o projeto

---
+++ Randomizer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Randomizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> randomizedList = args.Randomize();
+
+            foreach (string item in randomizedList)
+            {
+                Console.Out.WriteLine(item);
+            }
+        }
+    }
+
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Randomize<T>(this IEnumerable<T> alvo)
+        {
+            Random random = new Random();
+            return alvo.OrderBy(x => (random.Next()));
+        }
+    }
+}
